01c6140eff4a0075dfb9ef9ec542442d5974a16f
angelal8 <59952281+angelal8@users.noreply.github.com>
2020-04-16 23:36:09 -1000

    1

--- Lee_Angela_Assignment1/public/server.js
+++ Lee_Angela_Assignment1/public/server.js
@@ -11,37 +11,3 @@ function isNonNegInt(q,returnErrors = false) {
 
 let products_array=require('./pastries.json');
 /* Used code from Professor Dan's assignment 1 workshop file & lab 13 and modified from there */
-var express = require('express');
-var app = express();
-var myParser = require("body-parser");
-var fs=require('fs');
-
-var data=require('./public/product_data.js');
-var products=data.products;
-
-app.all('*',function(request,response,next) {
-    console.log(request.method + ' to ' + request.path);
-    next();
-}
-);
-
-app.use(myParser.urlencoded({ extended: true }));
-
-app.post('/quantity_form', function (request, response) {
-    let POST=request.body;
-    q=POST["quantity_textbox"];
-
-    if(type POST['quantity_textbox'] != 'undefined') {
-        console.log(q);
-        if(isNonNegInt(q,false)) {
-            response.send('Purchased ${q} cakes!');
-        }
-        else {
-            response.send(`${q} is not valid. Try again.`);
-        }
-    };
-    
-});
-
-app.use(express.static('.public'));
-app.listen(8080, () => console.log(`connected to port 8080`));
